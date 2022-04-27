18f368eed9d90608a18b80b3df4742993e60da1b
dainiuxt <dainiuxt@gmail.com>
2022-04-27 19:26:04 +0300

    Add request; Handle pagination

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 import requests
 from lxml import html
 from modules.store import StoreItem
+import pandas as pd
 
 link = 'https://www.cvbankas.lt/?padalinys[0]=76&page=1'
+adds = list()
 
-# print(source.xpath("//span[contains(@class, 'salary_amount')]/text()"))
+def get_pages(page_url):
+  r = requests.get(page_url)
+  r.encoding = r.apparent_encoding
+  source = html.fromstring(r.content)
+  page_list = source.xpath("//ul[contains(@class, 'pages_ul_inner')]/li/a/text()")
+  return page_list
 
 def get_source(page_url):
   r = requests.get(page_url)
+  r.encoding = r.apparent_encoding
   source = html.fromstring(r.content)
-  # return source
-  article_list = source.xpath("//article")
-  return article_list
-  
-print(len(get_source(link)))
+  articles = source.xpath("//article")
+  return articles
+
+pages = get_pages(link)
+
+for page in range(int(pages[-1])):
+  link = f'https://www.cvbankas.lt/?padalinys[0]=76&page={page+1}'
+  article_list = get_source(link)
+
+  for article in article_list:
+    title = article.xpath(".//h3/text()")
+    link = article.xpath(".//a[contains(@class, 'list_a')]/@href")
+    calculations = article.xpath(".//div[contains(@class, 'jobadlist_list_cell_salary')]/text()")
+    city = article.xpath(".//span[contains(@class, 'list_city')]/text()")
+    old = article.xpath(".//span[contains(@class, 'txt_list_2')]/text()")
+
+    if len(calculations) == 1:
+      netgross = ['']
+      salary = ['']
+    else:
+      salary = article.xpath(".//span[contains(@class, 'salary_amount')]/text()")
+      netgross = article.xpath(".//span[contains(@class, 'salary_calculation')]/text()")
+
+    add = StoreItem(title, link, salary, city, netgross, old)
+    adds.append(add)
+
+print(len(adds))
--- modules/store.py
+++ modules/store.py
@@ -1,7 +1,9 @@
 class StoreItem:
 
-   def __init__(self, name, link, salary, city):
-       self.name = name
+   def __init__(self, title, link, salary, city, netgross, old):
+       self.old = old
+       self.title = title
        self.link = link
        self.salary = salary
+       self.netgross = netgross
        self.city = city
